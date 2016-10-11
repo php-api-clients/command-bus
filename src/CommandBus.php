aa851059c0377984dee054a460aeeee645b42f77
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-10-11 21:37:47 +0200

    Taking the command bus to awesome town: http://image.slidesharecdn.com/commandbustoawesometown-150626151125-lva1-app6892/95/command-bus-to-awesome-town-2-638.jpg

---
+++ src/CommandBus.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tools\CommandBus;
+
+use League\Tactician\CommandBus as Tactician;
+use League\Tactician\Handler\CommandHandlerMiddleware;
+use React\EventLoop\LoopInterface;
+use React\Promise\CancellablePromiseInterface;
+use function WyriHaximus\React\futurePromise;
+use function React\Promise\resolve;
+
+final class CommandBus
+{
+    /**
+     * @var Tactician
+     */
+    private $commandBus;
+
+    /**
+     * @var LoopInterface
+     */
+    private $loop;
+
+    /**
+     * @param LoopInterface $loop
+     * @param CommandHandlerMiddleware $commandHandlerMiddleware
+     */
+    public function __construct(LoopInterface $loop, CommandHandlerMiddleware $commandHandlerMiddleware)
+    {
+        $this->loop = $loop;
+        $this->commandBus = new Tactician([
+            $commandHandlerMiddleware,
+        ]);
+    }
+
+    /**
+     * Executes the given command and optionally returns a value
+     *
+     * @param object $command
+     *
+     * @return mixed
+     */
+    public function handle($command): CancellablePromiseInterface
+    {
+        return futurePromise($this->loop, $command)->then(function ($command) {
+            return resolve($this->commandBus->handle($command));
+        });
+    }
+}
---
+++ tests/CommandBusTest.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Tools\CommandBus;
+
+use ApiClients\Tools\TestUtilities\TestCase;
+use League\Tactician\Handler\CommandHandlerMiddleware;
+use League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor;
+use League\Tactician\Handler\Locator\InMemoryLocator;
+use League\Tactician\Handler\MethodNameInflector\HandleInflector;
+use React\EventLoop\Factory;
+use function Clue\React\Block\await;
+use function React\Promise\resolve;
+
+final class CommandBusTest extends TestCase
+{
+    public function testHandle()
+    {
+        $loop = Factory::create();
+
+        $command = new class('t-dog') {
+            private $t;
+            public function __construct(string $t)
+            {
+                $this->t = $t;
+            }
+
+            public function getT(): string
+            {
+                return $this->t;
+            }
+        };
+
+        $commandToHandlerMap = [
+            get_class($command) => new class() {
+                public function handle($command)
+                {
+                    return resolve($command);
+                }
+            },
+        ];
+
+        $handlerMiddleware = new CommandHandlerMiddleware(
+            new ClassNameExtractor(),
+            new InMemoryLocator($commandToHandlerMap),
+            new HandleInflector()
+        );
+
+        $commandBus = new CommandBus($loop, $handlerMiddleware);
+
+        $this->assertSame(
+            $command,
+            await(
+                $commandBus->handle($command),
+                $loop
+            )
+        );
+    }
+}
